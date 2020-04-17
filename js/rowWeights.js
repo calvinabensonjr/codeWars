56e7572771c0f0a0f38bc783aa470baf56655124
resilient-coders35 <resilient-coders35@resilient-new05s-MacBook-Pro.local>
2020-04-17 17:12:44 -0400

    update

--- js/rowWeights.js
+++ js/rowWeights.js
@@ -1,3 +1,12 @@
+Scenario
+Several people are standing in a row divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+Task
+Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+
+
+
 function rowWeights(array){
 rowWeights=arr=>arr.reduce((a,b,i)=>(a[i%2]+=b,a),[0,0])
 
